2a7b7533b07eb30fdd440a5f138f2e5ba1b3687a
Muhammad Nor kholit <99956928+muhammadnorkholit@users.noreply.github.com>
2024-01-06 17:41:32 +0700

    Update NavbarContainer.tsx

--- app/components/navbar/front/NavbarContainer.tsx
+++ app/components/navbar/front/NavbarContainer.tsx
@@ -4,16 +4,16 @@ import { cookies } from "next/headers";
 export default function NavbarContainer() {
   let cookie = cookies();
   let name = "";
-  if (cookie.has("auth")) {
-    const authCookie = cookie.get("auth")?.value;
-    if (authCookie) {
-      try {
-        const parsedData = JSON.parse(authCookie);
-        name = parsedData.name || "";
-      } catch (error) {
-        console.error("Error parsing JSON:", error);
-      }
-    }
-  }
+  // if (cookie.has("auth")) {
+  //   const authCookie = cookie.get("auth")?.value;
+  //   if (authCookie) {
+  //     try {
+  //       const parsedData = JSON.parse(authCookie);
+  //       name = parsedData.name || "";
+  //     } catch (error) {
+  //       console.error("Error parsing JSON:", error);
+  //     }
+  //   }
+  // }
   return <Navbar dataAuth={name} />;
 }
