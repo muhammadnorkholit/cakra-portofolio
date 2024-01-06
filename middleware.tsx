553bfe36e5997054ceb75da22d66d3070c8ba480
Muhammad kholit <99956928+muhammadnorkholit@users.noreply.github.com>
2024-01-06 19:51:42 +0700

    update middleware

--- middleware.tsx
+++ middleware.tsx
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   if (
-    !request.cookies.has("auth") &&
+    !request.cookies.has("auth") ||
     request.cookies.get("auth")?.value == ""
   ) {
     return NextResponse.redirect(new URL("/login", request.url));
